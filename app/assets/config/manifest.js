8de63693b9e294634ae90544df051140337d7f39
trndav <farsiteguide@gmail.com>
2024-01-04 18:23:56 +0100

    Add new dice and dice type drag&drop do not work

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link sortablejs/Sortable.min.js
+//= link stimulus-character-counter/dist/stimulus-character-counter.umd.js
+//= link stimulus-sortable/dist/stimulus-sortable.umd.js
