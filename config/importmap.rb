140b4cf9e54a72780e71311b4a2dca824a6a5f4e
trndav <farsiteguide@gmail.com>
2024-01-04 13:52:07 +0100

    Importmap.rb fix?

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,10 +2,11 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "stimulus-character-counter", to: "stimulus-character-counter.js"
-pin "stimulus-sortable", to: "stimulus-sortable.js"
-pin "sortablejs", to: "sortablejs.js"
-pin "@rails/request.js", to: "rails-request.js"
+
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.8
+pin "sortablejs" # @1.15.1
+pin "stimulus-character-counter" # @4.2.0
+pin "stimulus-sortable" # @4.1.1
